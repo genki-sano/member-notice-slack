cbef0463ca6ebefd58d95ef2e0f0ba40b1de0537
g-sano <g.sano.2441@gmail.com>
2021-10-10 23:50:59 +0900

    テスト用のメソッド作成

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,49 @@ global.doPost = (
 global.schedule = (): GoogleAppsScript.Content.TextOutput => {
   return schedule()
 }
+
+// NOTE: 誤って作成されたスケジュールを消すのに使用
+// global.resetSchedule = (): GoogleAppsScript.Content.TextOutput => {
+//   const token =
+//     PropertiesService.getScriptProperties().getProperty('SLACK_BOT_TOKEN')
+//   const response = UrlFetchApp.fetch(
+//     'https://www.slack.com/api/chat.scheduledMessages.list',
+//     {
+//       method: 'post',
+//       contentType: 'application/x-www-form-urlencoded',
+//       headers: { Authorization: `Bearer ${token}` },
+//       payload: {},
+//     },
+//   )
+//   const resp = JSON.parse(response.getContentText())
+
+//   resp.scheduled_messages.forEach(
+//     (item: { id: string; channel_id: string }) => {
+//       UrlFetchApp.fetch(
+//         'https://www.slack.com/api/chat.deleteScheduledMessage',
+//         {
+//           method: 'post',
+//           contentType: 'application/x-www-form-urlencoded',
+//           headers: { Authorization: `Bearer ${token}` },
+//           payload: {
+//             channel: item.channel_id,
+//             scheduled_message_id: item.id,
+//           },
+//         },
+//       )
+//     },
+//   )
+
+//   const check = UrlFetchApp.fetch(
+//     'https://www.slack.com/api/chat.scheduledMessages.list',
+//     {
+//       method: 'post',
+//       contentType: 'application/x-www-form-urlencoded',
+//       headers: { Authorization: `Bearer ${token}` },
+//       payload: {},
+//     },
+//   )
+//   Logger.log(JSON.parse(check.getContentText()))
+
+//   return ContentService.createTextOutput('')
+// }
